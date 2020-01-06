cd45a7f5c9342d28308052d6c28a1e4b277786d5
rabbitate <56537513+rabbitate@users.noreply.github.com>
2020-01-06 13:13:33 +0900

    Create Prob_01.java

---
+++ Prob_01.java
@@ -0,0 +1,11 @@
+// 1번
+
+public class Prob_01 {
+	public static void main(String[] Args) {
+		int op1 = 10, op2 = -20, sum;
+		sum = op1 + op2;
+		System.out.printf("%d + %d = %d\n", op1, op2, sum);
+		System.out.printf("%3d + %d = %d\n", op1, op2, sum);				
+		System.out.printf("%d %% %d = %2d\n", op1, 3, op1 % 3);
+	}
+}
